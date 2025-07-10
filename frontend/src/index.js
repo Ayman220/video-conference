00c215247de329e5162ea2bfb1f6bf89a4c02520
Ayman Audi <aymanoudi88@gmail.com>
2025-07-10 15:35:45 +0400

    Add polyfill for Node.js 'process' global in index.js to ensure browser compatibility

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,5 @@
+// Polyfill Node.js 'process' global for browser compatibility
+window.process = { env: {} };
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
